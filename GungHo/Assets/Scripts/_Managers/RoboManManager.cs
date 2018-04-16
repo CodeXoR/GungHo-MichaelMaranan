e67f4a5ecb0688056b7b715f951f7d2c30d3eae4
Michael Maranan <michaelthommaranan@gmail.com>
2018-04-16 03:57:19 -0700

    Scene Transitions implemented.

---
+++ GungHo/Assets/Scripts/_Managers/RoboManManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoboManManager : GameManager {
+
+	void Update() {
+
+		if (SceneReady == false) {
+			return;
+		}
+
+		if (currentScene == "DemoLevel") {
+			if (Input.GetButtonDown ("Submit")) {
+				ReloadScene ();
+			} 
+			else if (Input.GetButtonDown ("Cancel")) {
+				LoadScene ("StartMenu");
+			}
+		} 
+		else if(currentScene == "StartMenu") {
+			if (Input.GetButtonDown ("Submit")) {
+				LoadScene ("DemoLevel");
+			} 
+			else if (Input.GetButtonDown ("Cancel")) {
+				#if UNITY_STANDALONE || UNITY_STANDALONE_OSX
+				Application.Quit ();
+				#endif
+			}
+		}
+	}
+}
